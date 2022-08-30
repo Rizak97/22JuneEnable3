1a66ab741261fbe516d02f8d4504228c5734cba9
abdirizak osman <abdirizakosman123@gmail.com>
2022-08-30 17:27:25 +0100

    initial commit

---
+++ REACT/react001/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import pl from "./header.jsx";
+
+function App() {
+  return (
+    
+    <h1>
+      <pl />
+    </h1>
+    
+
+      
+    
+  );
+}
+
+export default App;
+
